68a6f72266a8363eece52d5001ae4504bb81ef2d
Simone <sbaglivi@gmail.com>
2019-07-22 16:54:12 +0200

    fixed app output

--- app.js
+++ app.js
@@ -50,5 +50,5 @@ app.use(itemRoutes);
 //    console.log(`Currently listening on port ${3000}`);
 //})
 app.listen(process.env.PORT || 3000, process.env.IP, ()=>{
-    console.log(`Currently listening on port ${process.env.PORT}`);
+    console.log(`Currently listening on port ${process.env.PORT || 3000}`);
 });
